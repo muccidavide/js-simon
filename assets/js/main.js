f8a5702f448beab76dd0118f29043a883af87e91
muccidavide <davidemucci0804@gmail.com>
2022-04-06 15:04:53 +0200

    Add random number function

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/* 
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+// creo funzione per generare 5 numeri random
+
+
+let numberRandomArray = [];
+while (numberRandomArray.length < 5 ){
+    let numberRandom = Math.floor(Math.random() * 100) + 1;
+    if (!numberRandomArray.includes(numberRandom)) {
+        numberRandomArray.push(numberRandom)
+    }
+}
+// console.log(numberRandomArray);
+
+// li appendo ad un elemento della DOM
+
+
+
+// funzione affinchè numeri devono sparire dopo 30 secondi 
+
+// compare prompt dove utente deve inserire i 5 numeri 
+
+// confronto fra numeri apparsi e numeri inseirti dall'utente
+
+
+// a schermo numeri quali e quanti numeri indovinato dalll'utente.
